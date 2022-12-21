aee89a8dd9887bec068a61bcc59fdf7ddaa57eb8
Alberto Guzman-Alvarez <alvarez1991@gmail.com>
2022-12-21 14:02:32 -0800

    playing with the model building, i think its looking good :)

--- sandbox_gtp.R
+++ sandbox_gtp.R
@@ -109,6 +109,14 @@ covar_for_treatment <- union(covar_confound, covar_rel_treatment)
 covar_for_outcome <- union(covar_confound, covar_rel_outcome)
 
 
+
+
+
+
+
+
+
+
 #########################################
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # ~~ generate scenarios for population treatment models
@@ -116,58 +124,105 @@ covar_for_outcome <- union(covar_confound, covar_rel_outcome)
 #########################################
 
 
+#########################################
+# base model
+#########################################
+
+b <- sub(".*v", "", covar_for_treatment)
+element <- paste0("b", b, " * ", covar_for_treatment)
+
+# Concatenate the variables from covar_for_treatment into a single string
+equation <- paste0("(1 + exp(-(0 + ", paste(element, collapse = " + "), ")))^-1")
+
+# Evaluate the equation
+trueps <- eval(parse(text = equation))
 
 
-b <- sub(".*v","",covar_for_treatment)
-element <- paste0('b',b,' * ', covar_for_treatment)
-form <- paste("(1 + exp(-(0 + b1 * v1 +", paste(element, collapse = " + ")) 
 
 
 
 
-#cov_20 <- sample(element,((.2)*length(element)))
 
-if (scenarioT == "A") {
-  
-  #base model 
-  
-  f_form <- paste("(1 + exp(-(0 + b1 * v1 +", paste(element, collapse = " + "),")))^-1") 
-  trueps <- eval(parse(text = f_form))
-  
-} else
-  
-  if (scenarioT == "B") {
-    
-    #non-linear model
-    add_lin <- paste0(cov_20, '*',cov_20)
-    b_2 <- sub(".*v","",cov_20)
-    f_form2 <- paste(form," + ", paste0('b',b_2,'*',add_lin, collapse = " + "),")))^-1")
-    trueps <- eval(parse(text = f_form2))
-    
-  } else
-    
-    if (scenarioT == "C") {
-      
-      #non-additive model
-      n1 <- 0.5
-      add_cov <- sample(cov_80,((.2)*length(cov_80)))
-      b_3 <- sub(".*v","",add_cov)
-      b_4 <- sample(b,length(add_cov))
-      add <- paste0('b',b_3,'*',n1,'*','v',b_4,'*',add_cov)
-      f_form3 <- paste(form," + ", paste(add, collapse = " + "),")))^-1") 
-      trueps <- eval(parse(text = f_form3))
-      
-    } else
-      
-      if (scenarioT == "D") {
-        
-        #complex model
-        n1 <- 0.5
-        add_lin <- paste0(cov_20, '*',cov_20)
-        add_cov <- sample(cov_80,((.2)*length(cov_80)))
-        b_5 <- sub(".*v","",add_cov)
-        b_6 <- sample(b,length(add_cov))
-        add <- paste0('b',b_5,'*',n1,'*','v',b_6,'*',add_cov)
-        f_form4 <- paste(form," + ", paste(add_lin, ' + ',add, collapse = " + "),")))^-1") 
-        trueps <- eval(parse(text = f_form4))
-      }
+
+#########################################
+# non-linear model
+#########################################
+
+# Split covar_for_treatment into four equal-sized groups
+n <- length(covar_for_treatment)
+split_index <- round(seq(1, n, length.out = 4))
+covar_groups <- split(covar_for_treatment, cut(seq_along(covar_for_treatment), split_index))
+
+# Get the first group of covariates
+first_group <- covar_groups[[1]]
+
+# Iterate over the first group of covariates and create a quadratic term for each one
+quadratic_terms <- list()
+for (group in first_group) {
+  # Extract the coefficient from the covariate name
+  b <- sub(".*v", "", group)
+  # Create the quadratic term
+  quadratic_terms[[group]] <- paste0("b", b, " * ", group, "^2")
+}
+
+# Concatenate all of the terms together and store the result in a new variable called equation
+equation <- paste0("(1 + exp(-(0 + ", paste(c(unlist(quadratic_terms), element), collapse = " + "), ")))^-1")
+
+
+
+#########################################
+# non-additive model
+#########################################
+
+
+
+n <- length(covar_for_treatment)
+split_index <- round(seq(1, n, length.out = 4))
+covar_groups <- split(covar_for_treatment, cut(seq_along(covar_for_treatment), split_index))
+
+
+
+
+interaction_terms <- lapply(covar_groups, function(group) {
+  # Extract the coefficients from the covariate names
+  b1 <- sub(".*v", "", group[1])
+  b2 <- sub(".*v", "", group[2])
+  # Create the interaction term
+  paste0("b", b1, " * b", b2, " * ", group[1], " * ", group[2])
+})
+
+
+
+
+equation <- paste0("(1 + exp(-(0 + ", paste(c(unlist(interaction_terms), element), collapse = " + "), ")))^-1")
+
+
+
+
+
+#########################################
+# complex model
+#########################################
+
+
+
+
+# Split covar_for_treatment into four equal-sized groups
+n <- length(covar_for_treatment)
+split_index <- round(seq(1, n, length.out = 4))
+covar_groups <- split(covar_for_treatment, cut(seq_along(covar_for_treatment), split_index))
+
+# Iterate over each group of covariates and create both a quadratic term and an interaction term for each one
+terms <- list()
+for (group in covar_groups) {
+  # Extract the coefficients from the covariate names
+  b1 <- sub(".*v", "", group[1])
+  b2 <- sub(".*v", "", group[2])
+  # Create the quadratic term
+  terms[[group[1]]] <- paste0("b", b1, " * ", group[1], "^2")
+  # Create the interaction term
+  terms[[paste0(group[1], " * ", group[2])]] <- paste0("b", b1, " * b", b2, " * ", group[1], " * ", group[2])
+}
+
+# Concatenate all of the terms together and store the result in a new variable called equation
+equation <- paste0("(1 + exp(-(0 + ", paste(c(unlist(terms), element), collapse = " + "), ")))^-1")
