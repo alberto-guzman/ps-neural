deea80fe016f7796d9361e6797e21d33375b2f0d
Alberto Guzman-Alvarez <alvarez1991@gmail.com>
2022-12-21 09:42:45 -0800

    created correlated variables between 0 and .6 that retain original correlations...almost

--- sandbox_gtp.R
+++ sandbox_gtp.R
@@ -3,9 +3,10 @@ library(MASS)
 library(Matrix)
 library(psych)
 library(MBESS)
+library(Rlab)
 
 # Set the number of variables (p) and the sample size (n)
-p <- 100
+p <- 10
 n <- 1000
 
 # Generate a mean vector of 0s
@@ -32,18 +33,27 @@ vars <- mvrnorm(n, mean, cov)
 
 # Calculate the number of normal, Bernoulli, and uniform variables to generate
 num_norm_vars <- floor(p / 2)
-num_bernoulli_vars <- floor(p / 4)
-num_uniform_vars <- p - num_norm_vars - num_bernoulli_vars
+num_bern_vars <- floor(p / 4)
+num_uniform_vars <- p - num_norm_vars - num_bern_vars
 
-# Convert the first num_norm_vars variables to normal variables
-vars_normal <- qnorm(pnorm(vars[, 1:num_norm_vars]), mean=mean[1:num_norm_vars], sd=sd[1:num_norm_vars])
+# Convert all variables to uniform variables between 0 and 1
+vars_unif <- pnorm(vars)  
 
-# Convert the next num_bernoulli_vars variables to Bernoulli variables
-vars_bernoulli <- qbinom(pnorm(vars[, (num_norm_vars+1):(num_norm_vars+num_bernoulli_vars)]), 1, 0.5)
+# Convert the first num_norm_vars variables to normal variable with mean = 1 and sd = 1
+vars_normal <- qnorm(vars_unif[, 1:num_norm_vars])
 
-# Convert the remaining variables to uniform variables
-vars_uniform <- qunif(pnorm(vars[, (num_norm_vars+num_bernoulli_vars+1):p]), min=min(vars[, (num_norm_vars+num_bernoulli_vars+1):p]), max=max(vars[, (num_norm_vars+num_bernoulli_vars+1):p]))
+# Convert the next num_bern_vars variables to Bernoulli variables
+vars_bern <- qbern(vars_unif[, (num_norm_vars + 1):(num_norm_vars + num_bern_vars)], 0.5)
+
+# The remainder are left as uniform variables
+vars_uniform <- vars_unif[, (num_norm_vars + num_bern_vars + 1):p]
 
 # Combine the transformed variables
-vars_transformed <- cbind(vars_bernoulli, vars_uniform, vars_normal)
+vars_transformed <- cbind(vars_bern, vars_uniform, vars_normal)
+
+
+
+
+
+
 
