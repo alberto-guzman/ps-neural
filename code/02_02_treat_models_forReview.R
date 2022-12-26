5a3159b638d9201d99209d2d0c60831300de90e8
Alberto Guzman-Alvarez <alvarez1991@gmail.com>
2022-12-26 13:25:22 -0800

    treatment models working as expected :)

--- code/02_02_treat_models_forReview.R
+++ code/02_02_treat_models_forReview.R
@@ -9,10 +9,10 @@ beta <- vector("list", length(master_covar))
 for (i in seq_len(length(master_covar))) {
   # Generate a random beta value
   g <- round(rbeta(1, 1, 1), 2)
-  
+
   # Assign the value to a variable named b1, b2, etc.
   assign(paste0("b", i), g)
-  
+
   # Store the variable names in the beta list
   b <- paste0("b", i)
   beta[[i]] <- b
@@ -20,16 +20,6 @@ for (i in seq_len(length(master_covar))) {
 
 # Add the variables from vars_transformed to the global environment
 list2env(vars_transformed, .GlobalEnv)
-
-
-
-
-
-
-
-
-
-
 
 #########################################
 # Generate base model
@@ -46,17 +36,6 @@ equation <- paste0("(1 + exp(-(0 + ", paste(element, collapse = " + "), ")))^-1"
 
 # Evaluate the equation and store the result in trueps
 trueps <- eval(parse(text = equation))
-
-
-
-
-
-
-
-
-
-
-
 
 #########################################
 # Non-linear model
@@ -89,23 +68,16 @@ equation <- paste0("(1 + exp(-(0 + ", paste(c(unlist(quadratic_terms), element),
 # Evaluate the equation and store the result in trueps
 trueps <- eval(parse(text = equation))
 
-
-
-
-
-
-
-
-
-
 #########################################
 # non-additive model
 #########################################
 
+# Split covar_for_treatment into four equal-sized groups
 n <- length(covar_for_treatment)
 split_index <- round(seq(1, n, length.out = 4))
 covar_groups <- split(covar_for_treatment, cut(seq_along(covar_for_treatment), split_index))
 
+# Iterate over each group of covariates and create an interaction term between the two covariates
 interaction_terms <- lapply(covar_groups, function(group) {
   # Extract the coefficient from the covariate name
   b <- sub(".*v", "", group[1])
@@ -113,13 +85,14 @@ interaction_terms <- lapply(covar_groups, function(group) {
   paste0("b", b, " * ", group[1], " * ", group[2])
 })
 
+# Concatenate all of the terms together and store the result in a new variable called equation
 equation <- paste0("(1 + exp(-(0 + ", paste(c(unlist(interaction_terms), element), collapse = " + "), ")))^-1")
 
 # Evaluate the equation
 trueps <- eval(parse(text = equation))
 
 #########################################
-# complex model
+# Complex model
 #########################################
 
 # Split covar_for_treatment into four equal-sized groups
@@ -133,7 +106,7 @@ for (group in covar_groups) {
   # Extract the coefficients from the covariate names
   b <- sub(".*v", "", group[1])
   # Create the quadratic term
-  terms[[group[1]]] <- paste0("b", b1, " * ", group[1], "^2")
+  terms[[group[1]]] <- paste0("b", b, " * ", group[1], "^2")
   # Create the interaction term
   terms[[paste0(group[1], " * ", group[2])]] <- paste0("b", b, " * ", group[1], " * ", group[2])
 }
