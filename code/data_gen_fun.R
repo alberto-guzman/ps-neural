24a2a6615a8eb9b9db8dec5fb347a49a5ffa7c76
Alberto Guzman-Alvarez <alvarez1991@gmail.com>
2023-01-01 11:04:35 -0800

    data gen function complete :) now to test it!

--- code/data_gen_fun.R
+++ code/data_gen_fun.R
@@ -14,7 +14,7 @@ library(MBESS)
 library(Rlab)
 library(tidyverse)
 
-generate_dat <- function(n, p, scenarioT) {
+generate_dat <- function(n, p, scenarioT, scenarioY) {
   # Generate a mean vector of 0s
   mean <- rep(0, p)
 
@@ -110,7 +110,7 @@ generate_dat <- function(n, p, scenarioT) {
   element <- paste0("b", b, " * ", covar_for_treatment)
 
   #########################################
-  # Generate base model
+  # Population treatment model - Generate base model
   #########################################
   if (scenarioT == "A") {
     # Concatenate the variables from covar_for_treatment into a single string
@@ -121,7 +121,7 @@ generate_dat <- function(n, p, scenarioT) {
   } else
 
   #########################################
-  # Non-linear model
+  # Population treatment model - Non-linear model
   #########################################
   if (scenarioT == "B") {
     # Split covar_for_treatment into four equal-sized groups
@@ -151,7 +151,7 @@ generate_dat <- function(n, p, scenarioT) {
   } else
 
   #########################################
-  # Non-additive model
+  # Population treatment model - Non-additive model
   #########################################
   if (scenarioT == "C") {
     # Split covar_for_treatment into four equal-sized groups
@@ -175,7 +175,7 @@ generate_dat <- function(n, p, scenarioT) {
   } else
 
   #########################################
-  # Complex model
+  # Population treatment model - Complex model
   #########################################
   if (scenarioT == "D") {
     # Split covar_for_treatment into four equal-sized groups
@@ -201,18 +201,167 @@ generate_dat <- function(n, p, scenarioT) {
     trueps <- eval(parse(text = equation))
   }
 
+  
   #########################################
-  # Generate binary treatment T
+  # ~~ binary treatment T
   #########################################
-
+  
   unif1 <- runif(n, 0, 1)
   T <- ifelse(trueps > unif1, 1, 0) # there is a probability of unif1 that T=1
+  
+  
+  # Generate a coefficients for population outcome models
+  # Initialize a0 to 1
+  a0 <- 1
+  # Initialize treatment effect g to 0.3
+  g <- 0.3
+  
+  alpha <- vector("list", length(master_covar))
+  
+  for (i in 1:length(master_covar)) {
+    # Generate a random beta value
+    g <- round(runif(1, min = -0.5, max = 0.5), 2)
+    # Assign the value to a1, a2, a3, etc.
+    assign(paste0("a", i), g)
+    a <- paste0("a", i)
+    alpha[[i + 1]] <- a
+  }
+  
+  # Extract the coefficient from the covariate name
+  a <- sub(".*v", "", covar_for_outcome)
+  
+  # Create a new variable called element with the format "a * covar_for_outcome"
+  element <- paste0("a", a, " * ", covar_for_outcome)
+  
+  
+  
+  #########################################
+  # Population outcome model - Generate base model
+  #########################################
+  if (scenarioY == "a") {
+    # Concatenate the variables from covar_for_outcome into a single string
+    equation_Y <- paste0("a0 + g * T ", " + ", paste(element, collapse = " + "))
+    
+    # Evaluate the equation
+    Y <- eval(parse(text = equation_Y))
 
-  return(trueps)
-  #return(T)
+  } else
+    
+    #########################################
+  # Population outcome model - Non-linear model
+  #########################################
+  if (scenarioY == "b") {
+    # Split covar_for_outcome into four equal-sized groups
+    n <- length(covar_for_outcome)
+    split_index <- round(seq(1, n, length.out = 4))
+    covar_groups <- split(covar_for_outcome, cut(seq_along(covar_for_outcome), split_index))
+    
+    # Get the first group of covariates
+    first_group <- covar_groups[[1]]
+    
+    # Create an empty list to store the quadratic terms
+    quadratic_terms <- list()
+    
+    # Iterate over each group of covariates and create an interaction term between the two covariates
+    for (group in first_group) {
+      # Extract the coefficient from the covariate name
+      a <- sub(".*v", "", group)
+      # Create the quadratic term with the format "a * covar^2"
+      quadratic_terms[[group]] <- paste0("a", a, " * ", group, "^2")
+    }
+    
+    equation_Y <- paste0("a0 + g * T + ", paste(c(unlist(quadratic_terms), element), collapse = " + "), "")
+    
+    # Evaluate the equation
+    Y <- eval(parse(text = equation_Y))
+    
+  } else
+    
+    #########################################
+  # Population outcome model - Non-additive model
+  #########################################
+  if (scenarioY == "d") {
+    # Split covar_for_outcome into four equal-sized groups
+    n <- length(covar_for_outcome)
+    split_index <- round(seq(1, n, length.out = 4))
+    covar_groups <- split(covar_for_outcome, cut(seq_along(covar_for_outcome), split_index))
+    
+    # Iterate over each group of covariates and create an interaction term between the two covariates
+    interaction_terms <- lapply(covar_groups, function(group) {
+      # Extract the coefficient from the covariate name
+      a <- sub(".*v", "", group[1])
+      # Create the interaction term
+      paste0("a", a, " * ", group[1], " * ", group[2])
+    })
+    
+    equation_Y <- paste0("a0 + g * T + ", paste(c(unlist(interaction_terms), element), collapse = " + "), "")
+    
+    # Evaluate the equation
+    Y <- eval(parse(text = equation_Y))
+   
+  } else
+    
+    #########################################
+  # Population outcome model - Complex model
+  #########################################
+  if (scenarioY == "d") {
+    
+    # Split covar_for_outcome into four equal-sized groups
+    n <- length(covar_for_outcome)
+    split_index <- round(seq(1, n, length.out = 4))
+    covar_groups <- split(covar_for_outcome, cut(seq_along(covar_for_outcome), split_index))
+    
+    # Iterate over each group of covariates and create both a quadratic term and an interaction term for each one
+    terms <- list()
+    for (group in covar_groups) {
+      # Extract the coefficients from the covariate names
+      a <- sub(".*v", "", group[1])
+      # Create the quadratic term
+      terms[[group[1]]] <- paste0("a", a, " * ", group[1], "^2")
+      # Create the interaction term
+      terms[[paste0(group[1], " * ", group[2])]] <- paste0("a", a, " * ", group[1], " * ", group[2])
+    }
+    
+    # Concatenate all of the terms together and store the result in a new variable called equation
+    equation_Y <- paste0("a0 + g * T + ", paste(c(unlist(terms), element), collapse = " + "), "")
+    
+    
+    # Evaluate the equation
+    Y <- eval(parse(text = equation_Y))
+    
+  }
+  
+  #########################################
+  # Form simulated data tibble
+  #########################################
+  
+  v_list <- mget(paste0("v", 1:length(master_covar)))
+  sim <- as_tibble(v_list)
+  sim$T <- T
+  sim$Y <- Y
+  sim$trueps <- trueps
+  mean_T <- mean(T)
+  return(mean_T)
+  
 }
+  
+#df <- generate_dat(n = 100000, p = 20, scenarioT = "A", scenarioY = "a")
 
 
-T <- generate_dat(n = 100000, p = 100, scenarioT = "D")
-hist(T)
-mean(T)
+
+set.seed(16)
+test <- replicate(n = 100, 
+          expr = generate_dat(n = 100000, p = 20, scenarioT = "D", scenarioY = "a"), 
+          simplify = FALSE )
+mean(unlist(test))
+
+# PS Avg for A = .30
+# PS Avg for B = .30
+# PS Avg for C = .29
+# PS Avg for D = .29
+
+
+# T Avg for A = .30
+# T Avg for B = .30
+# T Avg for C = .29
+# T Avg for D = .29
