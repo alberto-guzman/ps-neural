e81838c3afd56a3a49c10a1d7e5c0f0b8d861376
Alberto Guzman-Alvarez <alvarez1991@gmail.com>
2023-01-06 20:07:07 -0800

    use gpt to comment

--- code/psmethod_fun_sandbox.R
+++ code/psmethod_fun_sandbox.R
@@ -6,110 +6,123 @@
 
 
 
+# function to estimate the ATT and other metrics
 ps_methods <- function(data, method) {
+  # create a data frame from the data
   df <- data
 
+  # if the method is logit, then estimate the ATT using logistic regression
   if (method == "logit") {
+    # estimate the propensity score using logistic regression
     mod <- glm(T ~ . - Y - trueps, data = df, family = binomial)
+    # save the propensity score to a vector
     ps <- mod$fitted
+  # if the method is cart, then estimate the ATT using classification and regression trees
   } else if (method == "cart") {
+    # estimate the propensity score using classification and regression trees
     mod <- rpart(T ~ . - Y - trueps, method = "class", data = df)
+    # save the propensity score to a vector
     ps <- predict(mod)[, 2]
+  # if the method is bag, then estimate the ATT using bagging
   } else if (method == "bag") {
+    # estimate the propensity score using bagging
     mod <- bagging(T ~ . - Y - trueps, data = df, nbag = 100)
+    # save the propensity score to a vector
     ps <- predict(mod, newdata = df, type = "prob")
+  # if the method is forest, then estimate the ATT using random forest
   } else if (method == "forest") {
+    # estimate the propensity score using random forest
     mod <- randomForest(factor(T) ~ . - Y - trueps, ntree = 500, data = df)
+    # save the propensity score to a vector
     ps <- predict(mod, type = "prob")[, 2]
   }
 
 
-  # Save estimated propensity score and weights to data frame
+  # save estimated propensity score and weights to data frame
   df <- df %>%
     mutate(
       ps_pred = ps,
       ps_weights = if_else(T == 0, ps / (1 - ps), 1)
     )
 
-  # estimates the ATT with the weights
+  # estimate the ATT with the weights
   d.w <- svydesign(~1, weights = df$ps_weights, data = df)
   fit <- svyglm(Y ~ T, design = d.w)
 
-  # saves the ATT and se_ATT
+  # save the ATT and se_ATT
   ATT <- unname(coef(fit)["T"])
   vcov_matrix <- vcov(fit)
   ATT_se <- unname(sqrt(vcov_matrix["T", "T"]))
 
-  # calculate bias metrics
+  # calculate the bias metrics
   Bias <- ATT - 0.3
   AbsBias <- abs(ATT - 0.3)
 
-  # calculate mean of control group weights
+  # calculate the mean of control group weights
   df_int <- subset(df, T == 0)
   mean_ps_weights <- mean(df_int$ps_weights, na.rm = TRUE)
 
-  # calculate 95% coverage
+  # calculate the 95% coverage
   lower_bound <- ATT - 1.96 * ATT_se
   upper_bound <- ATT + 1.96 * ATT_se
   ci_95 <- ifelse(lower_bound <= 0.3 && 0.3 <= upper_bound, 1, 0)
 
   ###############
-  # calculate ASAM for covariates
+  # calculate the ASAM for covariates
   ###############
 
-  # Subset the data into the treatment and comparison groups
+  # subset the data into the treatment and comparison groups
   treatment_group <- df[df$T == 1, ]
   comparison_group <- df[df$T == 0, ]
 
-  # Get the names of the variables that start with "v"
+  # get the names of the variables that start with "v"
   var_names <- names(df)[grep("^v", names(df))]
 
-  # Initialize the ASAM_list vector
+  # initialize the ASAM_list vector
   ASAM_list <- rep(NA, length(var_names))
 
-  # Loop through each covariate
+    # loop through each covariate
   for (i in 1:length(var_names)) {
-    # Get the covariate name
+    # get the covariate name
     covariate <- var_names[i]
 
-    # Extract the covariate data from the treatment and comparison groups
+    # extract the covariate data from the treatment and comparison groups
     treatment_data <- treatment_group[[covariate]]
     comparison_data <- comparison_group[[covariate]]
 
-    # Extract the weights from the treatment and comparison groups
+    # extract the weights from the treatment and comparison groups
     treatment_weights <- treatment_group$ps_weights
     comparison_weights <- comparison_group$ps_weights
 
-    # Calculate the means of the treatment and comparison groups
+    # calculate the means of the treatment and comparison groups
     treatment_mean <- weighted.mean(treatment_data, treatment_weights)
     comparison_mean <- weighted.mean(comparison_data, comparison_weights)
 
-    # Calculate the variances of the treatment and comparison groups
+    # calculate the variances of the treatment and comparison groups
     treatment_var <- wtd.var(treatment_data, treatment_weights)
     comparison_var <- wtd.var(comparison_data, comparison_weights)
 
-    # Calculate the standard deviations of the treatment and comparison groups
+    # calculate the standard deviations of the treatment and comparison groups
     treatment_sd <- sqrt(treatment_var)
     comparison_sd <- sqrt(comparison_var)
 
-    # Calculate the standardized difference of means
+    # calculate the standardized difference of means
     sd_diff <- (treatment_mean - comparison_mean) / treatment_sd
 
-    # Take the absolute value of the standardized difference of means
+    # take the absolute value of the standardized difference of means
     abs_sd_diff <- abs(sd_diff)
 
-    # Save the absolute standardized difference of means in the ASAM_list vector
+    # save the absolute standardized difference of means in the ASAM_list vector
     ASAM_list[i] <- abs_sd_diff
   }
 
 
-  # Calculate the mean of the absolute standardized differences of means
+  # calculate the mean of the absolute standardized differences of means
   ASAM <- mean(ASAM_list)
 
 
   return(c(ATT, ATT_se, Bias, AbsBias, mean_ps_weights, ci_95, ASAM))
 }
-
 
 
 
