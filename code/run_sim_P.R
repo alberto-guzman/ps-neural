c476282ac5d38b15485afdd5671f8d2ae28209d5
Alberto Guzman-Alvarez <alg223@pitt.edu>
2023-01-26 18:06:32 -0800

    sending banch of 100 P

--- code/run_sim_P.R
+++ code/run_sim_P.R
@@ -56,10 +56,10 @@ use_virtualenv("/ihome/xqin/alg223/.virtualenvs/r-reticulate")
 
 res <- runSimulation(
   design = Design,
-  replications = 500,
+  replications = 100,
   generate = Generate,
   analyse = Analyse,
   summarise = Summarise,
   parallel = T,
-  filename = "sim_results_n10000_r500_P.rds"
+  filename = "sim_results_n10000_r100_P.rds"
 )
