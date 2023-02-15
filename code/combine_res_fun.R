ca1bb2a2a46138fa4cdf27fd40aa7e1c18000e58
Alberto Guzman-Alvarez <alg223@pitt.edu>
2023-02-15 11:49:20 -0800

    combined file and going to analyze it using SimHelpers in the quarto doc

--- code/combine_res_fun.R
+++ code/combine_res_fun.R
@@ -1,8 +1,4 @@
-library(tibble)
-library(dplyr)
-library(simhelpers)
-library(rsimsum)
-library(ggplot2)
+library(tidyverse)
 
 combine_results <- function(dir_path) {
   # Get a list of all the "results-row-*.rds" files in the directory
@@ -25,17 +21,4 @@ result1 <- combine_results("~/Projects/inProgress/2018_propensity_neuralnet_pape
 result2 <- combine_results("~/Projects/inProgress/2018_propensity_neuralnet_paper/data/sim_results_n10000_r1000_NP_e_all/")
 
 res <- bind_rows(result1, result2)
-
-saveRDS(res, file = "res_all.rds")
-
-s1 <- simsum(data = res, estvarname = "ATE", true = 0.3, se = "ATE_se", methodvar = "method", by = c("p"), ref = "logit", x = TRUE)
-s2 <- simsum(data = res, estvarname = "ATE", true = 0.3, se = "ATE_se", methodvar = "method", by = c("p", "scenarioT", "scenarioY"), ref = "logit", x = TRUE)
-
-summary(s1)
-
-autoplot(summary(s1), type = "forest", stats = "bias")
-autoplot(summary(s1), type = "lolly", stats = "bias")
-
-autoplot(s1, type = "zip")
-autoplot(s1, type = "est_density")
-autoplot(s1, type = "est_ridge") + xlim(0, .4)
+# res <- saveRDS(res)
